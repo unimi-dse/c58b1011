f90d0f7e440c6f7e7a5e6a523b0f332e53b438e2
1725bra <ebrimaboye111@gmail.com>
2020-02-16 23:57:28 +0100

    conflict check

--- R/INFIR.R
+++ R/INFIR.R
@@ -1,20 +1,18 @@
-#  @title Effects of Inflation on Interest Rate
-library(ggplot2)
-
+#'  @title Effects of Inflation on Interest Rate
 
  INFIR<-function(){
 
-   #Data contains all your CSV content.
-  Data <- read.csv(file ="C:/Users/39351/Documents/R/INFIR/INFIR.csv")
+  systemFileName <- system.file("extdata", "INFIR.csv", package = "INFIR")
+  Data <- read.csv(systemFileName)
 
   print(Data)
   class(Data)
   t<-c("Date","Rir","Inf1")
 
-  #Data not Date
+
   colnames(Data)<-t
 
-  #I guess Inf that your name your column is the same name with R default variable so I add 1 after Inf
+
   lmInf1 <- lm(Inf1~Rir , data=Data)
 
  l<-summary(lmInf1)
@@ -22,20 +20,12 @@ library(ggplot2)
 
   plot(Data, main = "scatterplot")
 
-
-  ##now you have data that contain Date and statistics - Data
-
-
-  #graph 1 shows Date and Rir
-  #you need a data.frame that contain Date and Rir. I name it g1
   g1 <- data.frame(Data$Date, Data$Rir)
-  plot(g1, type = "o")
+  plot(g1, type = "o", main = "Interest Rate fluctuation from 2005-2014")
   g2<-data.frame(Data$Date, Data$Inf1)
-  plot(g2, type= "o")
+  plot(g2, type= "o", main= "Inflation Trend 2005-2014")
   g3<-data.frame(Data$Rir, Data$Inf1)
-  plot(g3,type="o")
-
-
+  plot(g3,type="o" , main="Inresest Rate and Inflation Trend")
 
 }
 
